13c75aea2931fa7c785f9097da920dfd1691f707
vink77 <vink.av@mail.ru>
2023-11-01 00:58:45 +0700

    1

--- school/models.py
+++ school/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from users.models import NULLABLE
+from users.models import NULLABLE, User
 
 
 # Create your models here.
@@ -8,6 +8,8 @@ class Kurs(models.Model):
     kurs_name = models.CharField(max_length=100, verbose_name='название курса')
     kurs_avatar = models.ImageField(upload_to='school/', verbose_name='картинка', **NULLABLE)
     kurs_description = models.TextField(**NULLABLE, verbose_name='описание')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, **NULLABLE)
+
 
     def __str__(self):
         return f'{self.kurs_name}'
@@ -22,6 +24,7 @@ class Lesson(models.Model):
     lesson_avatar = models.ImageField(upload_to='school/', verbose_name='картинка урока', **NULLABLE)
     lesson_description = models.TextField(**NULLABLE, verbose_name='описание урока')
     video_url = models.URLField( **NULLABLE, verbose_name='ссылка на видео')
+    kurs = models.ForeignKey(Kurs, related_name='lesson', on_delete=models.SET_NULL, **NULLABLE)
 
 
     def __str__(self):
@@ -30,3 +33,21 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = 'урок'
         verbose_name_plural = 'уроки'
+
+class Pay(models.Model):
+    method = ('наличные',  'перевод на счет')
+
+
+    user = models.ForeignKey(User,related_name='user', on_delete=models.SET_NULL, **NULLABLE)
+    datapay = models.DateTimeField(verbose_name='дата оплаты', **NULLABLE)
+    kurspay = models.ForeignKey(Kurs,related_name='kurspay', on_delete=models.SET_NULL, verbose_name='оплата курса', **NULLABLE)
+    lessonpay = models.ForeignKey(Lesson,related_name='lessonpay', on_delete=models.SET_NULL,verbose_name='оплата урока', **NULLABLE)
+    payment = models.FloatField(verbose_name='сумма оплаты', **NULLABLE)
+    paymentmethod = models.CharField(max_length=35, method=method, verbose_name='способ оплаты')
+
+    def __str__(self):
+        return f'{self.user} ({self.payment})'
+
+    class Meta:
+        verbose_name = 'платеж'
+        verbose_name_plural = 'платежи'
