c626eaf274a60bd6ab8ec0d85d9655fb1d5e41f7
vink77 <vink.av@mail.ru>
2023-11-08 23:50:58 +0700

    исправлена ошибка, добавлены миграции

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-м
+
 NULLABLE = {"null": True, "blank": True}
 # Create your models here.
 
